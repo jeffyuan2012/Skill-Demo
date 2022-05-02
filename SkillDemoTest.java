9147c10fb416775fb4f257ee727d7f4b62e03770
jeffyuan2012 <60635786+jeffyuan2012@users.noreply.github.com>
2022-05-01 18:02:44 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,7 +5,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1,1)); // passes!
+        assertEquals(2, SkillDemo.add(1,2)); // Fail!
     }
 }
 
